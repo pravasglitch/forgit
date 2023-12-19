6cf311f2bb8440c3c3b3f65d4946210046cea32c
Pravas <pravasgurung82@gmail.com>
2023-12-19 21:14:01 +0545

    4th commit topi kinyo

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-st.write('Hello world')
+st.header('Hello world', divider='rainbow')
 st.header('My header')
 st.sidebar.write('I am a side bar')
 st.sidebar.write('trying again')
+st.sidebar.button('click me')
